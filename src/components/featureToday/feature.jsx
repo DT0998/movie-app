16e963cd60a04d5701a064ff524b1bc82baf7125
DT0998 <duongkytrieuvn@gmail.com>
2022-01-23 20:12:10 +0700

    feature update

--- src/components/featureToday/feature.jsx
+++ src/components/featureToday/feature.jsx
@@ -18,11 +18,10 @@ export const Feature = () => {
       let response = await axios.get(API_URL);
       let data = response.data;
       setMovietrending(data.results);
-      console.log(data)
+      console.log(data);
     };
     getTrending();
   }, [API_URL]);
-
 
   return (
     <div className="wrap_fluid feature">
@@ -30,31 +29,33 @@ export const Feature = () => {
         <Row>
           <Col>
             <div className="wrap">
-              <div className="d-flex">
-                  <h1> Trending </h1>
-                  <h1> Trending </h1>
+              <div className="d-flex justify-content-between align-items-center">
+                <h1 className="trending_title"> Trending </h1>
+                <button class="btn_view view-more">
+                  <span class="circle" aria-hidden="true">
+                    <span class="icon arrow"></span>
+                  </span>
+                  <span class="button-text">View More</span>
+                </button>
               </div>
               <div className=" d-flex flex-row gap-3">
-                  {movietrending.map((movie,index)=> index < 5 &&(
-                <Card className="card_container" key={movie.id}>
-                  <img
-                    src={IMG_URL + movie.poster_path}
-                    alt={movie.original_name}
-                    className="img_feature card-img-top"
-                  />
-                  <div className="card-body card_trending">
-                    <p className="card-text">
-                      {movie.title}
-                    </p>
-                    <p className="card-text">
-                      {movie.release_date}
-                    </p>
-                    <p className="card-text">
-                      {movie.vote_average}
-                    </p>
-                  </div>
-                </Card>
-                  ))}
+                {movietrending.map(
+                  (movie, index) =>
+                    index < 5 && (
+                      <Card className="card_container" key={movie.id}>
+                        <img
+                          src={IMG_URL + movie.poster_path}
+                          alt={movie.original_name}
+                          className="img_feature card-img-top"
+                        />
+                        <div className="card-body card_trending">
+                          <p className="card-text">{movie.title}</p>
+                          <p className="card-text">{movie.release_date}</p>
+                          <p className="card-text">{movie.vote_average}</p>
+                        </div>
+                      </Card>
+                    )
+                )}
               </div>
             </div>
           </Col>
