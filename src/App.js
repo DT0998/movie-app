85ca1a4768be4e709f18b56125c86a411b46d4ab
DT0998 <duongkytrieuvn@gmail.com>
2022-01-23 15:04:28 +0700

    header fix

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { Feature } from './components/featureToday/feature';
 import { Movie } from './components/movie/movie';
 import { Footer } from './components/footer/footer';
 import { Tvshow } from './components/tv-show/tvshow';
+import { Nav } from './components/nav/nav';
 
 
 function App() {
   return (
     <div>
+      <Nav/>
       <Header/>
       <Feature />
       <Movie />
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,34 +1,72 @@
 import "./header.css";
-import { Nav } from "../nav/nav";
-import { HeaderSlider } from "./headerSlider";
 
+import { Row, Col, Container } from "react-bootstrap";
+// swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/scrollbar";
 import { useEffect, useState } from "react";
 import axios from "axios";
+
 
 export const Header = () => {
   // api
   const API_KEY = "api_key=82cdb0894626ba4286c1d6bd41791249";
   const BASE_URL = "https://api.themoviedb.org/3";
   const API_URL = BASE_URL + "/movie/popular?" + API_KEY;
-  const IMG_ORG = "https://image.tmdb.org/t/p/original";
+  const IMG_URL = "http://image.tmdb.org/t/p/w500/";
+  const IMG_ORG = "https://image.tmdb.org/t/p/original/";
 
   // fetch movie api
-  const [moviepopularbg, setMoviepopularbg] = useState([]);
+  const [moviepopular, setMoviepopular] = useState([]);
 
   useEffect(() => {
-    async function getPopular() {
+    const getPopular = async function () {
       let response = await axios.get(API_URL);
       let data = response.data;
-      setMoviepopularbg(data.results);
-      console.log(data);
-    }
+      setMoviepopular(data.results);
+    };
     getPopular();
   }, [API_URL]);
 
   return (
-    <div className="wrap_fluid header">
-      <Nav />
-      <HeaderSlider />
-    </div>
+    <Swiper
+      autoplay={{
+        delay: 2000,
+      }}
+      className="header_slider"
+    >
+      {moviepopular.map((movie) => (
+        <SwiperSlide data-swiper-autoplay="2000" key={movie.id}>
+          <div
+            className="wrap_fluid header"
+            style={{ backgroundImage: `url(${IMG_ORG + movie.backdrop_path})` }}
+          >
+            <Container>
+              <Row className="d-flex align-items-center">
+                <Col xs={12} md={8}>
+                  <div className="wrap header_content">
+                    <div className="header_article">
+                      <h1>{movie.title}</h1>
+                      <p className="header_overview">{movie.overview}</p>
+                    </div>
+                    <button className="btn_watch">Watch Now</button>
+                  </div>
+                </Col>
+                <Col md={4}>
+                  <div className="wrap header_img">
+                    <img
+                      src={IMG_URL + movie.poster_path}
+                      alt={movie.title}
+                      className="img_movie"
+                    ></img>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
