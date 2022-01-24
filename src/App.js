5c6cae74964dee74ef8068d57dee80501ea4b908
vothecong <vtconga4tqtpy@gmail.com>
2022-01-24 15:54:49 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {Homepage} from './components/homepage/homepage';
+import { Homepage } from './components/homepage/homepage';
 import { Moviepage } from './components/moviepage/moviepage';
 import { Tvshowpage } from './components/tvshowpage/tvshowpage';
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div>
-    <Homepage/>
-    <Tvshowpage/>
-    <Moviepage/>
+      {/* <Homepage />
+      <Tvshowpage />
+      <Moviepage /> */}
+      {/* <Routes> */}
+      <Route path="/" exact component={Homepage} />
+      <Route path="/movie" component={Tvshowpage} />
+      <Route path="/tvshow" component={Moviepage} />
+      {/* </Routes> */}
     </div>
   );
 }
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -2,9 +2,8 @@ import "./nav.css";
 import logo from "../../assets/nav/logo/logo.svg";
 import { Container, Row, Col } from "react-bootstrap";
 import { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
 // route homepage
-import {Homepage} from '../homepage/homepage'
+import { Homepage } from '../homepage/homepage'
 // route detail
 import { Moviepage } from "../moviepage/moviepage";
 import { Tvshowpage } from "../tvshowpage/tvshowpage";
@@ -20,51 +19,47 @@ export const Nav = () => {
 
   return (
     <div>
-        <Container className={`nav_fluid ${scroll ? "sticky" : "nav_fluid"}`}>
-          <div className="wrap_fluid">
-            <div className="wrap">
-              <Row className="d-flex flex-row justify-content-center">
-                <Col className="col-lg-6">
-                  <div className="nav_left h-100">
-                    <ul className="container__list d-flex flex-row justify-content-md-start align-items-center gap-lg-3 h-100">
-                      <li>
-                        <a href="/">
-                          <img src={logo} className="logo" alt="logo"></img>
-                        </a>
-                      </li>
-                      <li>
-                        <a href="/tvshow">TV Show</a>
-                      </li>
-                      <li>
-                        <a href="/movie">Movie</a>
-                      </li>
-                    </ul>
-                  </div>
-                </Col>
-                <Col className="col-lg-6">
-                  <div className="nav_right h-100">
-                    <ul className="container__list d-flex flex-row justify-content-md-end align-items-center gap-lg-3 h-100">
-                      <li>
-                        <a href="/search">Search</a>
-                      </li>
-                      <li>
-                        <a href="/account">My Account</a>
-                      </li>
-                    </ul>
-                  </div>
-                </Col>
-              </Row>
-            </div>
+      <Container className={`nav_fluid ${scroll ? "sticky" : "nav_fluid"}`}>
+        <div className="wrap_fluid">
+          <div className="wrap">
+            <Row className="d-flex flex-row justify-content-center">
+              <Col className="col-lg-6">
+                <div className="nav_left h-100">
+                  <ul className="container__list d-flex flex-row justify-content-md-start align-items-center gap-lg-3 h-100">
+                    <li>
+                      <a href="/">
+                        <img src={logo} className="logo" alt="logo"></img>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/tvshow">TV Show</a>
+                    </li>
+                    <li>
+                      <a href="/movie">Movie</a>
+                    </li>
+                  </ul>
+                </div>
+              </Col>
+              <Col className="col-lg-6">
+                <div className="nav_right h-100">
+                  <ul className="container__list d-flex flex-row justify-content-md-end align-items-center gap-lg-3 h-100">
+                    <li>
+                      <a href="/search">Search</a>
+                    </li>
+                    <li>
+                      <a href="/account">My Account</a>
+                    </li>
+                  </ul>
+                </div>
+              </Col>
+            </Row>
           </div>
-          {/* route */}
-          </Container>
-          <Routes>
-              <Route path="/" element={<Homepage/>}></Route>
-              <Route path="/movie" element={<Moviepage/>}></Route>
-              <Route path="/tvshow" element={<Tvshowpage/>}></Route>
-          </Routes>
+        </div>
+        {/* route */}
+      </Container>
+
     </div>
 
-  
+
   );
 };
