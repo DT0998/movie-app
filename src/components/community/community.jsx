e15b64e0340249cb815849b2ed3df414dce8e0e2
DT0998 <duongkytrieuvn@gmail.com>
2022-01-24 11:29:18 +0700

    add api people

--- src/components/community/community.jsx
+++ src/components/community/community.jsx
@@ -2,15 +2,32 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 import "./community.css";
 // swiper
 import "swiper/css";
-import "swiper/css/scrollbar"
+import "swiper/css/scrollbar";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Scrollbar } from "swiper";
+import { useEffect, useState } from "react";
+import axios from "axios";
 SwiperCore.use([Scrollbar]);
 
-
 export const Community = () => {
+  // api
+  const API_KEY = "api_key=82cdb0894626ba4286c1d6bd41791249";
+  const BASE_URL = "https://api.themoviedb.org/3";
+  const API_URL = BASE_URL + "/person/popular?" + API_KEY;
+  const IMG_ORG = "https://image.tmdb.org/t/p/original/";
 
+  // fetch movie api
+  const [peoples, setPeoples] = useState([]);
 
+  useEffect(() => {
+    const getTrending = async function () {
+      let response = await axios.get(API_URL);
+      let data = response.data;
+      setPeoples(data.results);
+      console.log(data);
+    };
+    getTrending();
+  }, [API_URL]);
 
 
 
@@ -39,50 +56,31 @@ export const Community = () => {
                 scrollbar={{
                   " dragable": true,
                 }}
+                breakpoints={{
+                  // when window width is >= 320px
+                  320: {
+                    slidesPerView: 2,
+                  },
+                  // when window width is >= 480px
+                  480: {
+                    slidesPerView: 3,
+                  },
+                  // when window width is >= 640px
+                  640: {
+                    slidesPerView: 4,
+                  },
+                }}
               >
                 <div className="d-flex flex-column justify-content-around">
-                  <SwiperSlide>
-                    <Card className="card_community card_detail">
+                  {peoples.map((poeple)=>(
+                  <SwiperSlide key={poeple.id}>
+                    <Card className="card_community card_detail" style={{backgroundImage: `url(${IMG_ORG + poeple.profile_path})`}}>
                       <div className="border">
-                        <h2>Ben Stiller</h2>
+                        <h2>{poeple.name}</h2>
                       </div>
                     </Card>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                  <Card className="card_community card_detail">
-                      <div className="border">
-                        <h2>Ben Stiller</h2>
-                      </div>
-                    </Card>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                  <Card className="card_community card_detail">
-                      <div className="border">
-                        <h2>Ben Stiller</h2>
-                      </div>
-                    </Card>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                  <Card className="card_community card_detail">
-                      <div className="border">
-                        <h2>Ben Stiller</h2>
-                      </div>
-                    </Card>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                  <Card className="card_community card_detail">
-                      <div className="border">
-                        <h2>Ben Stiller</h2>
-                      </div>
-                    </Card>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                  <Card className="card_community card_detail">
-                      <div className="border">
-                        <h2>Ben Stiller</h2>
-                      </div>
-                    </Card>
-                  </SwiperSlide>
+                  </SwiperSlide>                                     
+                  ))}
                 </div>
               </Swiper>
             </div>
