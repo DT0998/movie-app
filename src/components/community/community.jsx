5c41bc0f85c203995d0540f41d2edfdaf9508636
DT0998 <duongkytrieuvn@gmail.com>
2022-01-24 08:56:53 +0700

    community add

--- src/components/community/community.jsx
+++ src/components/community/community.jsx
@@ -1,4 +1,4 @@
-import { Container,Row,Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import './community.css'
 
 export const Community = () => {
@@ -7,15 +7,39 @@ export const Community = () => {
             <Container>
                 <Row>
                     <Col>
-                        <div className="wrap">
+                        <div className='d-flex justify-content-between align-items-center'>
                             <h1>
                                 Community
                             </h1>
-                            <img src="https://via.placeholder.com/150" alt=''></img>
+                            <button className="btn_view view-more" data-aos="fade-left" data-aos-duration="1500">
+                                <span className="circle" aria-hidden="true">
+                                    <span className="icon arrow"></span>
+                                </span>
+                                <span className="button-text">View More</span>
+                            </button>
+                        </div>
+                        <div className="wrap bg_community">
+                            <div className='d-flex flex-column justify-content-around'>
+                                <div class="card_community card_detail">
+                                    <div class="border">
+                                        <h2>Ben Stiller</h2>
+                                    </div>
+                                </div>
+                                <div class="card_community card_detail">
+                                    <div class="border">
+                                        <h2>Ben Stiller</h2>
+                                    </div>
+                                </div>
+                                <div class="card_community card_detail">
+                                    <div class="border">
+                                        <h2>Ben Stiller</h2>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </Col>
                 </Row>
             </Container>
-        </div>
+        </div >
     );
 };
---
+++ src/components/movie-sort/moviesort.jsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Container, Col, Row, Card } from "react-bootstrap";
+import "./movie.css";
+
+export const Moviesort = () => {
+  // api
+  const API_KEY = "api_key=82cdb0894626ba4286c1d6bd41791249";
+  const BASE_URL = "https://api.themoviedb.org/3";
+  const API_URL = BASE_URL + "/movie/top_rated?" + API_KEY;
+  const IMG_URL = "http://image.tmdb.org/t/p/w500/";
+
+  // fetch movie api
+  const [movietoprate, setMovietoprate] = useState([]);
+
+  useEffect(() => {
+    const getTrending = async function () {
+      let response = await axios.get(API_URL);
+      let data = response.data;
+      setMovietoprate(data.results);
+      console.log(data);
+    };
+    getTrending();
+  }, [API_URL]);
+
+  return (
+    <div className="wrap_fluid feature">
+      <Container>
+        <Row>
+          <Col>
+            <div className="wrap">
+              <div className="d-flex justify-content-between align-items-center">
+                <h1 className="trending_title" data-aos="fade-right" data-aos-duration="1500"> MOVIES</h1>
+                <button className="btn_view view-more" data-aos="fade-left" data-aos-duration="1500">
+                  <span className="circle" aria-hidden="true">
+                    <span className="icon arrow"></span>
+                  </span>
+                  <span className="button-text">View More</span>
+                </button>
+              </div>
+              <div className=" d-flex flex-row gap-3" data-aos="fade-down" data-aos-duration="1500">
+                {movietoprate.map(
+                  (movie, index) =>
+                    index < 5 && (
+                      <Card className="card_container" key={movie.id}>
+                        <img
+                          src={IMG_URL + movie.poster_path}
+                          alt={movie.original_name}
+                          className="img_feature card-img-top"
+                        />
+                        <div className="card-body card_trending">
+                          <p className="card-text">{movie.title}</p>
+                          <p className="card-text">{movie.release_date}</p>
+                          <p className="card-text">{movie.vote_average}</p>
+                        </div>
+                      </Card>
+                    )
+                )}
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
