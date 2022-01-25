3f15ea792aa2985c0ba505bdc10b3d7fd27f2f50
DT0998 <duongkytrieuvn@gmail.com>
2022-01-25 15:32:48 +0700

    tvshow responsive

--- src/components/homepage/tv-show/tvshow.jsx
+++ src/components/homepage/tv-show/tvshow.jsx
@@ -3,9 +3,17 @@ import { useEffect, useState } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./tvshow.css";
+// media query hook
+import useMediaQuery from "../../../hooks/useMediaquery";
 
 export const Tvshow = () => {
   const [tvshows, setTVshows] = useState([]);
+
+    // media query
+    const isMobile = useMediaQuery("(min-width:320px)");
+    const isTablet = useMediaQuery("(min-width:768px)");
+    const isDesktop = useMediaQuery("(min-width:1024px)");
+  
   // api
   const API_KEY = "api_key=82cdb0894626ba4286c1d6bd41791249";
   const BASE_URL = "https://api.themoviedb.org/3";
@@ -24,12 +32,12 @@ export const Tvshow = () => {
   }, [API_URL]);
 
   return (
-    <div className="wrap_fluid feature">
+    <div className="wrap_fluid feature w-100">
       <Container>
         <Row>
           <Col>
             <div className="wrap">
-              <div className="d-flex justify-content-between align-items-center my-lg-3">
+              <div className="d-flex justify-content-between align-items-center my-3">
                 <h1 className="introduce_title" data-aos="fade-right" data-aos-duration="1500"> TV SHOWS</h1>
                 <Link to="/tvshow">
                   <button className="btn_view view-more" data-aos="fade-left" data-aos-duration="1500">
@@ -41,10 +49,11 @@ export const Tvshow = () => {
                 </Link>
               </div>
               <div className=" d-flex flex-row gap-3" data-aos="fade-down" data-aos-duration="1500">
-                {tvshows.map(
+                {/* desktop */}
+                {isDesktop ? tvshows.map(
                   (tvshow, index) =>
-                    index < 2 && (
-                      <Card className="card_container" key={tvshow.id}>
+                    index < 5 && (
+                      <Card className="card_container isDesktop" key={tvshow.id}>
                         <img
                           src={IMG_URL + tvshow.poster_path}
                           alt={tvshow.original_name}
@@ -57,7 +66,43 @@ export const Tvshow = () => {
                         </div>
                       </Card>
                     )
-                )}
+                ):null}
+                {/* tablet */}
+                {isTablet ? tvshows.map(
+                  (tvshow, index) =>
+                    index < 4 && (
+                      <Card className="card_container isTablet" key={tvshow.id}>
+                        <img
+                          src={IMG_URL + tvshow.poster_path}
+                          alt={tvshow.original_name}
+                          className="img_feature card-img-top"
+                        />
+                        <div className="card-body card_trending">
+                          <p className="card-text">{tvshow.original_name}</p>
+                          <p className="card-text">{tvshow.first_air_date}</p>
+                          <p className="card-text">{tvshow.vote_average}</p>
+                        </div>
+                      </Card>
+                    )
+                ):null}
+                {/* mobile */}
+                {isMobile ? tvshows.map(
+                  (tvshow, index) =>
+                    index < 2 && (
+                      <Card className="card_container isMobile" key={tvshow.id}>
+                        <img
+                          src={IMG_URL + tvshow.poster_path}
+                          alt={tvshow.original_name}
+                          className="img_feature card-img-top"
+                        />
+                        <div className="card-body card_trending">
+                          <p className="card-text">{tvshow.original_name}</p>
+                          <p className="card-text">{tvshow.first_air_date}</p>
+                          <p className="card-text">{tvshow.vote_average}</p>
+                        </div>
+                      </Card>
+                    )
+                ):null}
               </div>
             </div>
           </Col>
