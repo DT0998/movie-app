845fdab31cc99607c89dc58331380eb1bea39cbf
DT0998 <duongkytrieuvn@gmail.com>
2022-02-04 23:10:53 +0700

    update title movie legacy

--- src/pages/movielegacypage/movielegacypage.jsx
+++ src/pages/movielegacypage/movielegacypage.jsx
@@ -2,6 +2,7 @@
 import { Movielegacylist } from '../../components/movielegacy-list/movielegacylist';
 
 export const Movielegacypage = () => {
+  document.title = "Movie Legacy";
   return (
     <Movielegacylist />
   );
