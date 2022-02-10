088c5efc0f67a0019574b27d8258a532db51a33e
DT0998 <duongkytrieuvn@gmail.com>
2022-02-10 20:14:00 +0700

    Updates

--- src/components/buttons/button-watchnow/button-watchnow.jsx
+++
@@ -1,9 +0,0 @@
-import "./button-watchnow.css";
-
-export const Buttonwatchnow = () => {
-  return (
-    <div>
-      <button className="btn_watch">Watch Now</button>
-    </div>
-  );
-};
--- src/pages/movielegacypage/movielegacypage.jsx
+++
@@ -1,9 +0,0 @@
-// route movielegacypage
-import { Movielegacylist } from '../../components/movielegacy-list/movielegacylist';
-
-export const Movielegacypage = () => {
-  document.title = "Movie Legacy";
-  return (
-    <Movielegacylist />
-  );
-};
