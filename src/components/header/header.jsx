c5ee5d06c0d5ae2d17187aaa4c83d90c7bab40f4
DT0998 <duongkytrieuvn@gmail.com>
2022-04-03 14:58:35 +0700

    update header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -50,19 +50,23 @@ export const Header = () => {
           >
             <Container>
               <Row className="d-flex align-items-center">
-                <Col xs={12} md={8}>
-                  <div
-                    className="wrap header_content"
-                    data-aos="fade-right"
-                    data-aos-offset="300"
-                    data-aos-easing="ease-in-sine"
-                  >
+                <Col
+                  xs={12}
+                  md={8}
+                  data-aos="fade-right"
+                  data-aos-offset="300"
+                  data-aos-easing="ease-in-sine"
+                >
+                  <div className="wrap header_content">
                     <div className="header_article">
                       <h1 className="header_title">{movie.title}</h1>
                       <p className="header_overview">{movie.overview}</p>
                     </div>
                     <Link to={`/details/movie/${movie.id}`}>
-                     <Buttonsquare className="btn_info_watch" title="More info"/>
+                      <Buttonsquare
+                        className="btn_info_watch"
+                        title="More info"
+                      />
                     </Link>
                   </div>
                 </Col>
