3950ceb52a150fd7a0672de8888fbdb397dff455
DT0998 <duongkytrieuvn@gmail.com>
2022-04-03 15:01:45 +0700

    Updates

--- src/pages/movielegacypage/movielegacypage.jsx
+++
@@ -1,9 +0,0 @@
-// route movielegacypage
-import { Movielegacylist } from "../../components/showcase-list-card/list/movielegacy-list/movielegacy-list";
-
-export const Movielegacypage = () => {
-  document.title = "Movie Legacy";
-  return (
-    <Movielegacylist />
-  );
-};
