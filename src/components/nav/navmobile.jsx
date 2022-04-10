2b4bf665d7746a122b1e43de6c39d03da31c24c2
DT0998 <duongkytrieuvn@gmail.com>
2022-04-10 13:55:17 +0700

    update nav mobile

--- src/components/nav/navmobile.jsx
+++ src/components/nav/navmobile.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./navmobile.css";
-
+import { FaSearch } from "react-icons/fa";
 function NavMobile(props) {
   return (
     <React.Fragment>
       <div className="dropdown">
         <ul className="dropdown-content">
-          <li className="dropdown_title">
-            <Link to="/movie"> Movie </Link>
-          </li>
-          <li className="dropdown_title">
-            <Link to="/tvshow"> TV Shows </Link>
-          </li>
-          <li className="dropdown_title">
-            <Link to="/search"> Search </Link>
-          </li>
-          <li className="dropdown_title">
-            <Link to="/account"> My Account </Link>
-          </li>
+          <Link to="/movie">
+            <li className="dropdown_title">Movie</li>
+          </Link>
+          <Link to="/tvshow">
+            <li className="dropdown_title">TV Shows</li>
+          </Link>
+          <Link to="/search">
+            <li className="dropdown_title d-flex justify-content-between">
+              <div>Search</div>
+              <FaSearch />
+            </li>
+          </Link>
+          <Link to="/account">
+            <li className="dropdown_title">My Account</li>
+          </Link>
         </ul>
       </div>
     </React.Fragment>
