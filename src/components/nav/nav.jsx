0bc5925d8d84558bf0be9fdb79703f5d4d8daf0a
DT0998 <duongkytrieuvn@gmail.com>
2022-04-10 14:58:39 +0700

    update nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,7 +1,7 @@
 import "./nav.css";
 import logo from "../../assets/nav/logo/logo.svg";
 import { Container, Row, Col } from "react-bootstrap";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import useMediaQuery from "../../hooks/useMediaquery";
 import { FaBars } from "react-icons/fa";
@@ -31,86 +31,92 @@ export const Nav = (props) => {
   };
 
   return (
-    <Container className={`nav_fluid ${scroll && "sticky"} `}>
+    <React.Fragment>
       {isTablet ? (
         //  desktop
-        <div className="wrap_fluid">
-          <div className="wrap">
-            <Row className="d-flex flex-row justify-content-center nav_container">
-              <Col className="col-6">
-                <div className="nav_left h-100">
-                  <ul className="container__list d-flex flex-row justify-content-md-start align-items-center gap-3 h-100">
-                    <li>
-                      <Link to="/">
-                        <img src={logo} className="logo" alt="logo"></img>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/movie"> Movie </Link>
-                    </li>
-                    <li>
-                      <Link to="/tvshow"> TV Show </Link>
-                    </li>
-                  </ul>
-                </div>
-              </Col>
-              <Col className="col-6">
-                <div className="nav_right h-100">
-                  <ul className="container__list d-flex flex-row justify-content-md-end align-items-center gap-3 h-100">
-                    <li>
-                      <Link to="/search"> Search </Link>
-                    </li>
-                    <li>
-                      <Link to="/account"> My Account </Link>
-                    </li>
-                  </ul>
-                </div>
-              </Col>
-            </Row>
+        <Container className={`nav_fluid ${scroll && "sticky"}`}>
+          <div className="wrap_fluid">
+            <div className="wrap">
+              <Row className="d-flex flex-row justify-content-center nav_container">
+                <Col className="col-6">
+                  <div className="nav_left h-100">
+                    <ul className="container__list d-flex flex-row justify-content-md-start align-items-center gap-3 h-100">
+                      <li>
+                        <Link to="/">
+                          <img src={logo} className="logo" alt="logo"></img>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/movie"> Movie </Link>
+                      </li>
+                      <li>
+                        <Link to="/tvshow"> TV Show </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </Col>
+                <Col className="col-6">
+                  <div className="nav_right h-100">
+                    <ul className="container__list d-flex flex-row justify-content-md-end align-items-center gap-3 h-100">
+                      <li>
+                        <Link to="/search"> Search </Link>
+                      </li>
+                      <li>
+                        <Link to="/account"> My Account </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </Col>
+              </Row>
+            </div>
           </div>
-        </div>
+        </Container>
       ) : (
         // mobile
-        <div className="wrap_fluid">
-          <div className="wrap">
-            <Row className="d-flex flex-row justify-content-center nav_container">
-              <Col className="col-6">
-                <div className="nav_left h-100">
-                  <ul className="container__list d-flex flex-row justify-content-md-start align-items-center gap-3 h-100">
-                    <li>
-                      <Link to="/">
-                        <img src={logo} className="logo" alt="logo"></img>
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </Col>
-              <Col className="col-6">
-                <div className="nav_right h-100">
-                  <ul className="container__list d-flex flex-row justify-content-end align-items-center gap-3 h-100">
-                    {!isOpenNavMobile ? (
-                      <FaBars
-                        className="icons_menu"
-                        onClick={() => {
-                          setIsOpenNavMobile(!isOpenNavMobile);
-                        }}
-                      />
-                    ) : (
-                      <MdClose
-                        className="icons_menu"
-                        onClick={() => {
-                          setIsOpenNavMobile(!isOpenNavMobile);
-                        }}
-                      />
-                    )}
-                  </ul>
-                </div>
-              </Col>
-            </Row>
-            {isOpenNavMobile && <NavMobile setIsOpenNav={setIsOpenNavMobile} />}
-          </div>
-        </div>
+          <Container className={`nav_fluid ${scroll && "sticky"}`}>
+            <div className="wrap_fluid">
+              <div className="wrap">
+                <Row className="d-flex flex-row justify-content-center nav_container">
+                  <Col className="col-6">
+                    <div className="nav_left h-100">
+                      <ul className="container__list d-flex flex-row justify-content-md-start align-items-center gap-3 h-100">
+                        <li>
+                          <Link to="/">
+                            <img src={logo} className="logo" alt="logo"></img>
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </Col>
+                  <Col className="col-6">
+                    <div className="nav_right h-100">
+                      <ul className="container__list d-flex flex-row justify-content-end align-items-center gap-3 h-100">
+                        {!isOpenNavMobile ? (
+                          <FaBars
+                            className="icons_menu"
+                            onClick={() => {
+                              setIsOpenNavMobile(!isOpenNavMobile);
+                            }}
+                          />
+                        ) : (
+                          <MdClose
+                            className="icons_menu"
+                            onClick={() => {
+                              setIsOpenNavMobile(!isOpenNavMobile);
+                            }}
+                          />
+                        )}
+                      </ul>
+                    </div>
+                  </Col>
+                </Row>
+                {isOpenNavMobile && (
+                  <NavMobile setIsOpenNav={setIsOpenNavMobile} />
+                )}
+              </div>
+            </div>
+          </Container>
       )}
-    </Container>
+    </React.Fragment>
   );
 };
