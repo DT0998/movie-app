3c95fab4cb735ad8090fc60bfca947fcba971d95
DT0998 <duongkytrieuvn@gmail.com>
2022-06-24 17:59:57 +0700

    update form login module css

--- src/components/login/login-quotes/login-quotes.jsx
+++ src/components/login/login-quotes/login-quotes.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
+import classes from './login-quotes.module.css'
 import axios from "axios";
 
 export const Loginquotes = () => {
-  const [loginquotes, setLoginquotes] = useState([]);
+  const [loginQuotes, setLoginQuotes] = useState([]);
   // api
   const API_KEY = "api_key=82cdb0894626ba4286c1d6bd41791249";
   const BASE_URL = "https://api.themoviedb.org/3";
@@ -12,7 +13,7 @@ export const Loginquotes = () => {
   const getQuotes = async function () {
     let response = await axios.get(API_URL);
     let data = response.data;
-    setLoginquotes(data.results);
+    setLoginQuotes(data.results);
   };
   useEffect(() => {
     getQuotes();
@@ -20,14 +21,14 @@ export const Loginquotes = () => {
 
   return (
     <div>
-      {loginquotes.map(
+      {loginQuotes.map(
         (image, index) =>
           index < 1 && (
             <div
               style={{
                 backgroundImage: `url(${IMG_ORG + image.backdrop_path})`,
               }}
-              className="login_img"
+              className={classes.login_img}
               key={image.id}
             ></div>
           )
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,25 +1,28 @@
-import "./login.css";
-import { Container, Col, Row } from "react-bootstrap";
+import classes from "./login.module.css";
+import { Col, Row } from "react-bootstrap";
 import { Loginquotes } from "./login-quotes/login-quotes";
 import { Loginform } from "./login-form/login-form";
+import React from "react";
 
 function Login() {
   return (
-    <Container className="form_shadow px-5">
-      <div className="my-5">
+    <React.Fragment>
+      <div className={classes.form_container}>
+      <div className={`my-5 px-5 ${classes.form_shadow}`}>
         <Row className="d-flex flex-wrap justify-content-center align-items-center">
           <Col xs={12} className="text-center col-md-12 col-xl-12 col-xxl-6">
             <Loginform />
           </Col>
           <Col
             xs={0}
-            className="d-flex justify-content-center login_quotes col-md-0 col-xl-0 col-xxl-6"
+            className={`d-flex justify-content-center col-md-0 col-xl-0 col-xxl-6 ${classes.loginQuotes_container}`}
           >
             <Loginquotes />
           </Col>
         </Row>
       </div>
-    </Container>
+      </div>
+    </React.Fragment>
   );
 }
 
