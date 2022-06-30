0ecd5d43701a035cf50f76c3d7e917cd4bd41d07
DT0998 <duongkytrieuvn@gmail.com>
2022-06-30 10:52:08 +0700

    update loading

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -1,28 +1,17 @@
-import "./loading.css";
-import { Container, Row, Col } from "react-bootstrap";
-import Skeleton from 'react-loading-skeleton'
-import "react-loading-skeleton/dist/skeleton.css";
+import classes from "./loading.module.css";
+import React from "react";
 
 export const Loading = (props) => {
   return (
-    <Container>
-        <Row>
-          <Col>
-            {props.type === "fullscreen" ? (
-             <div className="wrap_fluid loading">
-              <div className="wrap d-flex justify-content-center align-items-center">
-                <div class="loader"></div>
-              </div>
-              </div>
-            ) : props.type === "button" ? (
-              <div className="wrap d-flex justify-content-center align-items-center">
-                <div class="loader"></div>
-              </div>
-            ) : props.type === "skeleton" ? <div className="wrap d-flex justify-content-center align-items-center">
-              <Skeleton duration={2}/>
-            </div> : null}
-          </Col>
-        </Row>
-      </Container>
+    <React.Fragment>
+      {props.type === "fullscreen" && 
+       <div className={`${classes.loading} d-flex justify-content-center align-items-center`}>
+          <div class={classes.loader}></div>
+        </div>}
+        {/* {props.type === "button" &&
+        <div className="wrap d-flex justify-content-center align-items-center">
+          <div class={classes.loader}></div>
+        </div>} */}
+    </React.Fragment>
   );
 };
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Header } from "../../components/header/header";
 import { Community } from "../../components/cast-community-recommend/community-slide/community-slide";
 import Showcase from "../../components/showcase-list/showcase";
+import { Loading } from "../../components/loading/loading";
 
 
 export const Homepage = () => {
@@ -13,9 +14,10 @@ export const Homepage = () => {
 
   return (
     <React.Fragment>
-      <Header />
+      <Loading type="fullscreen"/>
+      {/* <Header />
       <Showcase />
-      <Community />
+      <Community /> */}
     </React.Fragment>
   );
 };
