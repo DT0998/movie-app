c75247029cbb1f08bc13138b5ac2981ca0f73e91
DT0998 <duongkytrieuvn@gmail.com>
2022-07-14 11:05:38 +0700

    fix: add props spread in button square

--- src/components/buttons/button-square/index.jsx
+++ src/components/buttons/button-square/index.jsx
@@ -5,12 +5,12 @@ const ButtonSquare = (props) => {
   return (
     <React.Fragment>
       {props.type === "moreInfo" && (
-        <button className={classes.btn_info_watch} onClick={props.onClick}>
+        <button className={classes.btn_info_watch} {...props}>
           {props.title}
         </button>
       )}
       {props.type === "showMore" && (
-        <button className={classes.btn_loadmore} onClick={props.onClick}>
+        <button className={classes.btn_loadmore} {...props}>
           {props.title}
         </button>
       )}
