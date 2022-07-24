0ec8b264f0b8da8bf7c69c9d26e9226502713c62
DT0998 <duongkytrieuvn@gmail.com>
2022-07-24 22:51:54 +0700

    fix: remove react google login

--- src/components/login/login-form/index.jsx
+++ src/components/login/login-form/index.jsx
@@ -1,33 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import classes from "./login-form.module.css";
-import axios from "axios";
-// login google
-import GoogleLogin from "react-google-login";
 
 export const Loginform = () => {
-  const initialDataLogin = localStorage.getItem("loginData")
-    ? JSON.parse(localStorage.getItem("loginData"))
-    : null;
-  const [loginData, setLoginData] = useState(initialDataLogin);
-  const handleFailure = (response) => {
-    alert(response);
-  };
-  const handleSuccess = async (googleData) => {
-    const response = await axios.post("/api/login", {
-      body: JSON.stringify({
-        token: googleData.tokenId,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await response.json();
-    setLoginData(data);
-  };
-  const handleLogout = () => {
-    localStorage.removeItem("loginData");
-    setLoginData(null);
-  };
   return (
     <form>
       <div className={`${classes.loginForm} px-md-5`}>
@@ -36,21 +10,6 @@ export const Loginform = () => {
           <label className={classes.loginForm_content}>
             Login watch and chill movie
           </label>
-          {loginData ? (
-            <div>
-              <h3>You logged in as {loginData.email}</h3>
-              <button onClick={handleLogout}></button>
-            </div>
-          ) : (
-            <GoogleLogin
-              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-              onSuccess={handleSuccess}
-              onFailure={handleFailure}
-              buttonText="Login"
-              isSignedIn={true}
-              className={classes.btn_google}
-            />
-          )}
         </div>
       </div>
     </form>
