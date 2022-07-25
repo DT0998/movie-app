661cec2298a6c68099e7c15a149693ef966d515d
DT0998 <duongkytrieuvn@gmail.com>
2022-07-25 21:42:31 +0700

    fix: classname search bar

--- src/components/search-bar/index.jsx
+++ src/components/search-bar/index.jsx
@@ -1,16 +1,23 @@
-import classes from './search-bar.module.css';
+import classes from "./search-bar.module.css";
 import { FaSearch } from "react-icons/fa";
 function SearchInput() {
-    return (
-        <div className={`${classes.search_container} d-flex`}>
-        <div className="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Search for a movie..." aria-label="Search for a movie..."/>
+  return (
+    <div className={`${classes.search_container} d-flex`}>
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Search for a movie..."
+          aria-label="Search for a movie..."
+        />
         <div className="input-group-append d-flex">
-          <button class="btn btn-outline-secondary" type="button"><FaSearch/></button>
+          <button className="btn btn-outline-secondary" type="button">
+            <FaSearch />
+          </button>
         </div>
       </div>
-        </div>
-    );
+    </div>
+  );
 }
 
 export default SearchInput;
